32226a481206b91bb982d02aef9ff67f6142e805
NikolayLev <51372235+NikolayLev@users.noreply.github.com>
2020-01-13 17:26:40 +0300

    Update UserRoles.java

--- src/main/java/ru/levchenko/SimpleCrudDaoApp/models/UserRoles.java
+++ src/main/java/ru/levchenko/SimpleCrudDaoApp/models/UserRoles.java
@@ -8,15 +8,4 @@ public enum UserRoles {
 
     private static final Map<String, UserRoles> lookup = new HashMap<>();
 
-    static {
-        for (UserRoles roles : UserRoles.values()) {
-            lookup.put(roles.name(), roles);
-        }
-    }
-
-    public static UserRoles getRole(String role) {
-        return lookup.get(role);
-    }
-
-
 }
